1125d8591ef3bfbe203e7ad100894c662248f632
Ninhkx <ninhkx@mageplaza.com>
2018-11-30 08:59:35 +0700

    Add ACL

--- Block/Adminhtml/System/Config/Geoip.php
+++ Block/Adminhtml/System/Config/Geoip.php
@@ -32,93 +32,93 @@ use Mageplaza\GeoIP\Helper\Data as HelperData;
  */
 class Geoip extends Field
 {
-    /**
-     * @var string
-     */
-    protected $_template = 'Mageplaza_GeoIP::system/config/geoip.phtml';
+	/**
+	 * @var string
+	 */
+	protected $_template = 'Mageplaza_GeoIP::system/config/geoip.phtml';
 
-    /**
-     * @var HelperData
-     */
-    protected $_helperData;
+	/**
+	 * @var \Mageplaza\GeoIP\Helper\Data
+	 */
+	protected $_helperData;
 
-    /**
-     * Geoip constructor.
-     * @param Context $context
-     * @param HelperData $helperData
-     * @param array $data
-     */
-    public function __construct(
-        Context $context,
-        HelperData $helperData,
-        array $data = []
-    )
-    {
-        $this->_helperData = $helperData;
+	/**
+	 * Geoip constructor.
+	 * @param \Magento\Backend\Block\Template\Context $context
+	 * @param \Mageplaza\GeoIP\Helper\Data $helperData
+	 * @param array $data
+	 */
+	public function __construct(
+		Context $context,
+		HelperData $helperData,
+		array $data = []
+	)
+	{
+		$this->_helperData = $helperData;
 
-        parent::__construct($context, $data);
-    }
+		parent::__construct($context, $data);
+	}
 
-    /**
-     * Remove scope label
-     *
-     * @param AbstractElement $element
-     * @return string
-     */
-    public function render(AbstractElement $element)
-    {
-        $element->unsScope()->unsCanUseWebsiteValue()->unsCanUseDefaultValue();
+	/**
+	 * Remove scope label
+	 *
+	 * @param AbstractElement $element
+	 * @return string
+	 */
+	public function render(AbstractElement $element)
+	{
+		$element->unsScope()->unsCanUseWebsiteValue()->unsCanUseDefaultValue();
 
-        return parent::render($element);
-    }
+		return parent::render($element);
+	}
 
-    /**
-     * Return element html
-     *
-     * @param AbstractElement $element
-     * @return string
-     */
-    protected function _getElementHtml(AbstractElement $element)
-    {
-        return $this->_toHtml();
-    }
+	/**
+	 * Return element html
+	 *
+	 * @param AbstractElement $element
+	 * @return string
+	 */
+	protected function _getElementHtml(AbstractElement $element)
+	{
+		return $this->_toHtml();
+	}
 
-    /**
-     * Return ajax url for collect button
-     *
-     * @return string
-     */
-    public function getAjaxUrl()
-    {
-        return $this->getUrl('mpgeoip/system_config/geoip');
-    }
+	/**
+	 * Return ajax url for collect button
+	 *
+	 * @return string
+	 */
+	public function getAjaxUrl()
+	{
+		return $this->getUrl('mpgeoip/system_config/geoip');
+	}
 
-    /**
-     * Generate collect button html
-     *
-     * @return mixed
-     * @throws \Magento\Framework\Exception\LocalizedException
-     */
-    public function getButtonHtml()
-    {
-        $button = $this->getLayout()->createBlock(
-            'Magento\Backend\Block\Widget\Button'
-        )->setData(
-            [
-                'id'    => 'geoip_button',
-                'label' => __('Download Library'),
-            ]
-        );
+	/**
+	 * Generate collect button html
+	 *
+	 * @return mixed
+	 * @throws \Magento\Framework\Exception\LocalizedException
+	 */
+	public function getButtonHtml()
+	{
+		$button = $this->getLayout()->createBlock(
+			'Magento\Backend\Block\Widget\Button'
+		)->setData(
+			[
+				'id'    => 'geoip_button',
+				'label' => __('Download Library'),
+			]
+		);
 
-        return $button->toHtml();
-    }
+		return $button->toHtml();
+	}
 
-    /**
-     * @return string
-     * @throws \Magento\Framework\Exception\FileSystemException
-     */
-    public function isDisplayIcon()
-    {
-        return $this->_helperData->getAddressHelper()->checkHasLibrary() ? '' : 'hidden="hidden';
-    }
+	/**
+	 * @return string
+	 * @throws \Magento\Framework\Exception\FileSystemException
+	 */
+	public function isDisplayIcon()
+	{
+		return $this->_helperData->getAddressHelper()->checkHasLibrary() ? '' : 'hidden="hidden';
+	}
 }
--- Helper/Address.php
+++ Helper/Address.php
@@ -35,128 +35,128 @@ use Magento\Store\Model\StoreManagerInterface;
  */
 class Address extends Data
 {
-    /**
-     * @type \Magento\Framework\App\Filesystem\DirectoryList
-     */
-    protected $_directoryList;
+	/**
+	 * @type \Magento\Framework\App\Filesystem\DirectoryList
+	 */
+	protected $_directoryList;
 
-    /**
-     * @type \Magento\Framework\Locale\Resolver
-     */
-    protected $_localeResolver;
+	/**
+	 * @type \Magento\Framework\Locale\Resolver
+	 */
+	protected $_localeResolver;
 
-    /**
-     * @type \Magento\Directory\Model\Region
-     */
-    protected $_regionModel;
+	/**
+	 * @type \Magento\Directory\Model\Region
+	 */
+	protected $_regionModel;
 
-    /**
-     * @var CustomerAddressHelper
-     */
-    protected $addressHelper;
+	/**
+	 * @var CustomerAddressHelper
+	 */
+	protected $addressHelper;
 
-    /**
-     * Address constructor.
-     * @param Context $context
-     * @param ObjectManagerInterface $objectManager
-     * @param StoreManagerInterface $storeManager
-     * @param DirectoryList $directoryList
-     * @param Resolver $localeResolver
-     * @param Region $regionModel
-     */
-    public function __construct(
-        Context $context,
-        ObjectManagerInterface $objectManager,
-        StoreManagerInterface $storeManager,
-        DirectoryList $directoryList,
-        Resolver $localeResolver,
-        Region $regionModel
-    )
-    {
-        $this->_directoryList  = $directoryList;
-        $this->_localeResolver = $localeResolver;
-        $this->_regionModel    = $regionModel;
+	/**
+	 * Address constructor.
+	 * @param Context $context
+	 * @param ObjectManagerInterface $objectManager
+	 * @param StoreManagerInterface $storeManager
+	 * @param DirectoryList $directoryList
+	 * @param Resolver $localeResolver
+	 * @param Region $regionModel
+	 */
+	public function __construct(
+		Context $context,
+		ObjectManagerInterface $objectManager,
+		StoreManagerInterface $storeManager,
+		DirectoryList $directoryList,
+		Resolver $localeResolver,
+		Region $regionModel
+	)
+	{
+		$this->_directoryList  = $directoryList;
+		$this->_localeResolver = $localeResolver;
+		$this->_regionModel    = $regionModel;
 
-        parent::__construct($context, $objectManager, $storeManager);
-    }
+		parent::__construct($context, $objectManager, $storeManager);
+	}
 
-    /***************************************** Maxmind Db GeoIp ******************************************************/
-    /**
-     * Check has library at path var/Mageplaza/GeoIp/GeoIp/
-     * @return bool|string
-     * @throws \Magento\Framework\Exception\FileSystemException
-     */
-    public function checkHasLibrary()
-    {
-        $path = $this->_directoryList->getPath('var') . '/Mageplaza/GeoIp/GeoIp';
-        if (!file_exists($path)) {
-            return false;
-        }
+	/***************************************** Maxmind Db GeoIp ******************************************************/
+	/**
+	 * Check has library at path var/Mageplaza/GeoIp/GeoIp/
+	 * @return bool|string
+	 * @throws \Magento\Framework\Exception\FileSystemException
+	 */
+	public function checkHasLibrary()
+	{
+		$path = $this->_directoryList->getPath('var') . '/Mageplaza/GeoIp/GeoIp';
+		if (!file_exists($path)) {
+			return false;
+		}
 
-        $folder   = scandir($path, true);
-        $pathFile = $path . '/' . $folder[0] . '/GeoLite2-City.mmdb';
-        if (!file_exists($pathFile)) {
-            return false;
-        }
+		$folder   = scandir($path, true);
+		$pathFile = $path . '/' . $folder[0] . '/GeoLite2-City.mmdb';
+		if (!file_exists($pathFile)) {
+			return false;
+		}
 
-        return $pathFile;
-    }
+		return $pathFile;
+	}
 
-    /**
-     * @return array
-     * @throws \Magento\Framework\Exception\FileSystemException
-     */
-    public function getGeoIpData()
-    {
-        $libPath = $this->checkHasLibrary();
+	/**
+	 * @return array
+	 * @throws \Magento\Framework\Exception\FileSystemException
+	 */
+	public function getGeoIpData()
+	{
+		$libPath = $this->checkHasLibrary();
 
-        if ($this->getConfigValue('geoip/general/enable') && $libPath && class_exists('GeoIp2\Database\Reader')) {
-            try {
-                $geoIp  = new \GeoIp2\Database\Reader($libPath, $this->getLocales());
-                $record = $geoIp->city($this->_request->getParam('fakeIp', null) ?: $this->_remoteAddress->getRemoteAddress());
+		if ($this->getConfigValue('geoip/general/enable') && $libPath && class_exists('GeoIp2\Database\Reader')) {
+			try {
+				$geoIp  = new \GeoIp2\Database\Reader($libPath, $this->getLocales());
+				$record = $geoIp->city($this->_request->getParam('fakeIp', null) ?: $this->_remoteAddress->getRemoteAddress());
 
-                $geoIpData = [
-                    'city'       => $record->city->name,
-                    'country_id' => $record->country->isoCode,
-                    'postcode'   => $record->postal->code
-                ];
+				$geoIpData = [
+					'city'       => $record->city->name,
+					'country_id' => $record->country->isoCode,
+					'postcode'   => $record->postal->code
+				];
 
-                if ($record->mostSpecificSubdivision) {
-                    $code = $record->mostSpecificSubdivision->isoCode;
-                    if ($regionId = $this->_regionModel->loadByCode($code, $record->country->isoCode)->getId()) {
-                        $geoIpData['region_id'] = $regionId;
-                    } else {
-                        $geoIpData['region'] = $record->mostSpecificSubdivision->name;
-                    }
-                }
-                $allowedCountries = $this->getConfigValue('general/country/allow');
-                $allowedCountries = explode(',', $allowedCountries);
-                if (!in_array($geoIpData['country_id'], $allowedCountries)) {
-                    $geoIpData = [];
-                }
-            } catch (\Exception $e) {
-                $geoIpData = [];
-            }
+				if ($record->mostSpecificSubdivision) {
+					$code = $record->mostSpecificSubdivision->isoCode;
+					if ($regionId = $this->_regionModel->loadByCode($code, $record->country->isoCode)->getId()) {
+						$geoIpData['region_id'] = $regionId;
+					} else {
+						$geoIpData['region'] = $record->mostSpecificSubdivision->name;
+					}
+				}
+				$allowedCountries = $this->getConfigValue('general/country/allow');
+				$allowedCountries = explode(',', $allowedCountries);
+				if (!in_array($geoIpData['country_id'], $allowedCountries)) {
+					$geoIpData = [];
+				}
+			} catch (\Exception $e) {
+				$geoIpData = [];
+			}
 
-            return $geoIpData;
-        }
+			return $geoIpData;
+		}
 
-        return [];
-    }
+		return [];
+	}
 
-    /**
-     * @return array
-     */
-    protected function getLocales()
-    {
-        $locale   = $this->_localeResolver->getLocale();
-        $language = substr($locale, 0, 2) ? substr($locale, 0, 2) : 'en';
+	/**
+	 * @return array
+	 */
+	protected function getLocales()
+	{
+		$locale   = $this->_localeResolver->getLocale();
+		$language = substr($locale, 0, 2) ? substr($locale, 0, 2) : 'en';
 
-        $locales = [$language];
-        if ($language != 'en') {
-            $locales[] = 'en';
-        }
+		$locales = [$language];
+		if ($language != 'en') {
+			$locales[] = 'en';
+		}
 
-        return $locales;
-    }
+		return $locales;
+	}
 }
